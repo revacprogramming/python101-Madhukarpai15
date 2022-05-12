c38605cb8f58a00457e0a2377968229067d87cf4
Madhukar <96413903+Madhukarpai15@users.noreply.github.com>
2022-05-12 15:24:36 +0000

    7

--- ActivitySet01/problem07.py
+++ ActivitySet01/problem07.py
@@ -1,3 +1,15 @@
 # Strings
 
-text = "X-DSPAM-Confidence:    0.8475"
+def extract(a):
+    l=["0","1","2","3","4","5","6","7","8","9"]
+    for i in range(0,size):
+        if a[i] in l:
+            pos=a.find(a[i])
+            Str2=a[pos:size]
+            num=float(Str2)
+            return num
+
+Str="X-DSPAM-Confidence:    0.8475"
+size=len(Str)
+Num=extract(Str)
+print(Num)
