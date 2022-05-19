956d79b9c87e5507324126308e9c7c9ba4261334
Madhukar <96413903+Madhukarpai15@users.noreply.github.com>
2022-05-19 14:32:00 +0000

    12

--- ActivitySet01/problem12.py
+++ ActivitySet01/problem12.py
@@ -1,2 +1,16 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+
+import re
+
+fname=input("Enter the file name: ")
+fh=open(fname)
+Sum=0
+
+for line in fh:
+    strnum=re.findall('[0-9]+',line)
+    size=len(strnum)
+    for i in range(0,size):
+        Sum+=int(strnum[i])
+
+print(Sum)
