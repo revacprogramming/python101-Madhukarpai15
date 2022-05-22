22fb26302497540bce91a3286328091b1ae162c4
Madhukar <96413903+Madhukarpai15@users.noreply.github.com>
2022-05-22 06:59:10 +0000

    13

--- ActivitySet01/problem13.py
+++ ActivitySet01/problem13.py
@@ -18,3 +18,61 @@ while True:
 mysock.close()
 
 #Scraping HTML Data with BeautifulSoup
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+tags = soup('span')
+Sum=0
+
+for tag in tags:
+  Sum+=int(tag.contents[0])
+    
+print(Sum)
+
+#Following Links with BeautifulSoup
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+l=[]
+l2=[]
+l3=[]
+seq=[]
+i=0
+while i<=6:
+    if i==0:
+        url = input('Enter - ')
+        l3.append(url)
+    else:
+        url=l3[i]
+        
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    tags = soup('a')
+    for tag in tags:
+        x=tag.get('href', None)
+        y=tag.contents[0]
+        l.append(x)
+        l2.append(y)
+    l3.append(l[17])
+    seq.append(l2[17])
+    l.clear()
+    l2.clear()
+    i+=1
+
+print('Last name: ',seq[i-1])
