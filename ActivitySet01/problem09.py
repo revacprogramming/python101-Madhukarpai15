8b6e5ab30a401dc18e2e8999dfc68b14db673152
Madhukar <96413903+Madhukarpai15@users.noreply.github.com>
2022-05-24 17:10:08 +0000

    9

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,17 +1,38 @@
-# Lists
+#Files
 
-fname=input("Enter the file name: ")
-fh=open(fname)
+def Input():
+  fname=input("Enter the file name: ")
+  return fname
+
+def open_f(n):
+  fh=open(n)
+  return fh
+
 l=[]
 
-for line in fh:
-    x=line.split()
-    for word in x:
-        if word in l:
-            continue
-        else:
-            l.append(word)
+def find_num(b):
+  for line in b:
+      if line.startswith("X-DSPAM-Confidence:"):
+          pos=line.find(":")
+          flt=line[pos+3:]
+          flt2=float(flt)
+          l.append(flt2)
+      else:
+          continue
         
 
-l.sort()
-print(l)
+  size=len(l)
+  total=0
+  for i in range(0,size):
+      total+=l[i]
+  avg=total/size
+  return avg
+
+
+def output():
+  print("Average spam confidence: ",avg)
+
+fname=Input()
+fh=open_f(fname)
+avg=find_num(fh)
+output(avg)
