73b244766a5c8decfd01743c36ae8ab045c8d019
Madhukar <96413903+Madhukarpai15@users.noreply.github.com>
2022-05-24 16:36:28 +0000

    4

--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -1,18 +1,32 @@
 # Conditional Execution
 
-hrs1=input("Enter the number of hours you have worked: ")
-hrs=float(hrs1)
-rate=float(input("Enter the rate per hour: "))
+def inputhrs():
+  hrs1=input("Enter the number of hours you have worked: ")
+  hrs=float(hrs1)
+  return hrs
+
+def inputrate():
+  rate=float(input("Enter the rate per hour: "))
+  return rate
 
 
-if hrs>40:
+def compute_ot(a,b):
+  if hrs>40:
     pay1=hrs*rate
     hrs2=hrs-40
     rate1=rate*0.5
     pay2=hrs2*rate1
     pay=pay1+pay2
-    print("Your pay for the day is: ",pay)
-
-else:
+  
+  else:
     pay=hrs*rate
-    print("Your pay for the day is: ",pay)
+
+  return pay
+
+def output(c):
+  print('Your pay for the day is: ',pay)
+
+hrs=inputhrs()
+rate=inputrate()
+pay=compute_ot(hrs,rate)
+output(pay)
