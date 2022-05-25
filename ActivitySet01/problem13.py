a5d68d80f47996b42c75e51bb1a06a33b444e9e3
Madhukar <96413903+Madhukarpai15@users.noreply.github.com>
2022-05-25 03:48:36 +0000

    13

--- ActivitySet01/problem13.py
+++ ActivitySet01/problem13.py
@@ -1,78 +1,44 @@
-# Network Programming
-# https://www.py4e.com/lessons/network
+# Tuples
 
-#Request Response Cycle
-import socket
+def Input():
+  fname=input("Enter the file name: ")
+  return fname
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\n\n'.encode()
-mysock.send(cmd)
-
-while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
-        break
-    print(data.decode(),end='')
-
-mysock.close()
-
-#Scraping HTML Data with BeautifulSoup
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import ssl
-
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
-
-url = input('Enter - ')
-html = urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, "html.parser")
-
-tags = soup('span')
-Sum=0
-
-for tag in tags:
-  Sum+=int(tag.contents[0])
-    
-print(Sum)
-
-#Following Links with BeautifulSoup
-import urllib.request, urllib.parse, urllib.error
-from bs4 import BeautifulSoup
-import ssl
-
-
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
-
+def open_f(a):
+  fh=open(a)
+  return fh
+  
 l=[]
-l2=[]
-l3=[]
-seq=[]
-i=0
-while i<=6:
-    if i==0:
-        url = input('Enter - ')
-        l3.append(url)
-    else:
-        url=l3[i]
-        
-    html = urllib.request.urlopen(url, context=ctx).read()
-    soup = BeautifulSoup(html, 'html.parser')
+t1=()
+t2=()
 
-    tags = soup('a')
-    for tag in tags:
-        x=tag.get('href', None)
-        y=tag.contents[0]
-        l.append(x)
-        l2.append(y)
-    l3.append(l[17])
-    seq.append(l2[17])
-    l.clear()
-    l2.clear()
-    i+=1
+def hrs(b):
+  for line in b:
+      if line.startswith("From"):
+          pos=line.find(":")
+          time=line[pos-2:pos+6]
+          sepa=time.split(":")
+          hrs=sepa[0]
+          try:
+              hrs1=int(hrs)
+              l.append(hrs1)
+          except:
+              continue
+          if hrs1 not in t1:
+              t1=list(t1)
+              t1.append(hrs1)
+              t1=tuple(t1)
 
-print('Last name: ',seq[i-1])
+def output():
+  size=len(t1)
+  for i in range(0,size):
+      count=l.count(t1[i])
+      t2=list(t2)
+      t2.append(count)
+      t2=tuple(t2)
+      print(t1[i],  t2[i])
+
+fname=Input()
+fh=open_f(fname)
+hrs(fh)
+output()
