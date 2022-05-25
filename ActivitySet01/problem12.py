fd9e67e6ae1eab6728bf5668360c6bd3db4dee80
Madhukar <96413903+Madhukarpai15@users.noreply.github.com>
2022-05-25 03:29:45 +0000

    12

--- ActivitySet01/problem12.py
+++ ActivitySet01/problem12.py
@@ -1,16 +1,51 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+# Dictionaries
 
-import re
+def Input():
+  fname=input("Enter the file name: ")
+  return fname
+  
+def open_f(a):
+  fh=open(a)
+  return fh
+  
+l=[]
+d={}
+l1=[]
 
-fname=input("Enter the file name: ")
-fh=open(fname)
-Sum=0
+def mail_count(b):
+  for line in b:
+      if line.startswith("From"):
+          x=line.rstrip().split()
+          mail=x[1]
+          l.append(mail)
 
-for line in fh:
-    strnum=re.findall('[0-9]+',line)
-    size=len(strnum)
-    for i in range(0,size):
-        Sum+=int(strnum[i])
+  size=len(l)
+  for i in range(0,size):
+      count=l.count(l[i])/2
+      ele=l[i]
+      if ele in d:
+          continue
+      else:
+          d[ele]=count
+          l1.append(ele)
+    
+#dsize=len(d)
+  l1size=len(l1)
 
-print(Sum)
+  keys=list(d.keys())
+  vals=list(d.values())
+
+  maxkey=keys[vals.index(max(vals))]
+  maxval=max(vals)
+  vals=[]
+  vals.append(maxkey)
+  vals.append(maxval)
+  return vals
+
+def output(c):
+  print(c[0],c[1])
+
+fname=Input()
+fh=open_f(fname)
+result=mail_count(fh)
+output(result)
