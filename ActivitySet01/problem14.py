a4f906aaa39c5daaf0fa3503940d0084076181a9
Madhukar <96413903+Madhukarpai15@users.noreply.github.com>
2022-05-26 17:26:16 +0000

    19

--- ActivitySet01/problem14.py
+++ ActivitySet01/problem14.py
@@ -1,43 +1,29 @@
-# Using Web Services
-# https://www.py4e.com/lessons/servces
+# Regular Expressions
+# https://www.py4e.com/lessons/regex
 
-
-import urllib.request, urllib.parse, urllib.error
-import xml.etree.ElementTree as ET
-import ssl
 import re
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+def Input():
+  fname=input("Enter the file name: ")
+  return fname
 
+def open_f(a):
+  fh=open(a)
+  return fh
+  
 Sum=0
-l=[]
-while True:
-    address = input('Enter location: ')
-    if len(address) < 1: break
 
-    #parms = dict()
-    uh = urllib.request.urlopen(address, context=ctx)
-    data = uh.read()
-    tree = ET.fromstring(data)
-    results=tree.findall('.//count')
-    size=len(results)
-    for i in range(0,size):
-        Str=ET.tostring(results[i],encoding='utf8',method='xml').rstrip()
-        sstr=str(Str)
-        x=sstr.split()
-        
-        y=x[2].split()
-        
-        y2=str(y[0])
-        l.append(y2)
-      
-    size=len(l)
-    for i in range (0,size):
-        strnum=re.findall('[0-9]+',l[i])
-        Num=int(strnum[1])
-        Sum+=Num
-    break
-print(Sum)
+def Sum_num(b):
+  for line in b:
+      strnum=re.findall('[0-9]+',line)
+  size=len(strnum)
+  for i in range(0,size):
+      Sum+=int(strnum[i])
+
+def output():
+  print(Sum)
+
+fname=Input()
+fh=open_f(fname)
+Sum_num(fh)
+output()
