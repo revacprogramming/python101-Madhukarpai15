b8fcb130a2b133214f9a37a38b10a19c1eaf2dd4
Madhukar <96413903+Madhukarpai15@users.noreply.github.com>
2022-07-03 06:39:50 +0000

    3

--- ActivitySet02/problem03.py
+++ ActivitySet02/problem03.py
@@ -1,18 +1,24 @@
 
 
 def get_cs():
-    """get string input"""
+  """get string input"""
   a=input("Enter the string: ")
   return a
 
 
 def cs_to_lot(cs):
     """convert connected string to list of strings"""
+    final=[]
+    x=cs.split(';')
+    for i in x:
+        y=i.split('=')
+        z=tuple(y)
+        final.append(z)
+    return final
 
 
 def main():
     cs = get_cs()
-
     lot = cs_to_lot(cs)
     print(lot)
 
